3c9f9fccc99fd24aedca01f8c81e351b8b652fd7
ye6194 <ye6194@naver.com>
2024-07-26 17:51:12 +0900

    하드코딩으로 변형하기 이전
    - 모든 페이지를 긁어오려고 시도했지만 1페이지만 긁어짐

--- asdf.py
+++ asdf.py
@@ -3,8 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
 import time
 
@@ -22,11 +20,10 @@ time.sleep(2)
 search_box = driver.find_element(By.CSS_SELECTOR, "input.input_search")
 search_box.send_keys("서울특별시 라식 안과")
 search_box.send_keys(Keys.RETURN)
-time.sleep(3)
+time.sleep(5)
 
-# 병원 이름 리스트, url 리스트 초기화
+# 병원 이름 리스트 초기화
 hospital_names = []
-urls = []
 
 while True:
     # 스크롤을 내려서 모든 검색 결과 로드
@@ -45,9 +42,7 @@ while True:
 
     # 검색 결과에서 안과 병원 이름 추출
     results = soup.find_all("strong")
-    hospital_names.extend(
-        [result.get_text() for result in results if "안과" in result.get_text()]
-    )
+    hospital_names.extend([result.get_text() for result in results])
 
     # 다음 페이지 버튼 클릭
     try:
@@ -59,32 +54,40 @@ while True:
     except:
         break
 
+"""
+# BeautifulSoup를 사용하여 페이지 소스 파싱
+page_source = driver.page_source
+print()
+print()
+# print(page_source)
+print()
+print()
+
+# 스크롤을 내려서 모든 검색 결과 로드
+last_height = driver.execute_script("return document.body.scrollHeight")
+while True:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+
+# BeautifulSoup를 사용하여 페이지 소스 파싱
+page_source = driver.page_source
+soup = BeautifulSoup(page_source, "html.parser")
+
+# 검색 결과에서 안과 병원 이름 추출
+results = soup.find_all("strong", class_="marker_title")
+print(results)
+hospital_names = [result.get_text() for result in results]
+"""
+
 # 고유한 병원 이름 리스트 생성
-# unique_hospitals = set(hospital_names)
-# print(unique_hospitals)
-print(f"서울의 총 안과 수: {len(hospital_names)}")
-print("")
+unique_hospitals = set(hospital_names)
+total_hospitals = len(unique_hospitals)
 
-# 각 병원을 검색하고 URL 추출
-search_box.clear()
-for hospital in hospital_names:
-    search_box = driver.find_element(By.CSS_SELECTOR, "input.input_search")
-    search_box.clear()  # 검색창 비우기
-    time.sleep(5)  # clear 이후 잠시 대기
-
-    # 검색창이 비워졌는지 확인
-    while search_box.get_attribute("value") != "":
-        search_box.clear()
-        time.sleep(1)
-
-    search_box.send_keys(hospital)
-    search_box.send_keys(Keys.RETURN)
-    time.sleep(1)  # 페이지 로드 대기
-    urls.append(driver.current_url)
-
-# 총 안과 수와 각 병원의 URL 출력
-# for url in urls:
-# print(url)
+print(f"서울의 총 안과 수: {total_hospitals}")
 
 # 드라이버 종료
 driver.quit()
